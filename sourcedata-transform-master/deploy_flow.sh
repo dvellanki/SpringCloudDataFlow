94c07d974dc969f2702c16128f0ea10e423f9dd9
Divya Vellanki <divya.vellanki94@gmail.com>
2022-01-03 23:31:18 +0530

    Update deploy_flow.sh

--- sourcedata-transform-master/deploy_flow.sh
+++ sourcedata-transform-master/deploy_flow.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 docker build -t sourcedata-transform:1.0 .
-docker tag sourcedata-transform:1.0 harbor-repo.vmware.com/scdfrepo/sourcedata-transform:1.0
-docker login harbor-repo.vmware.com/scdfrepo
-docker push harbor-repo.vmware.com/scdfrepo/sourcedata-transform:1.0
+docker tag sourcedata-transform:1.0 repo.xxx.com/scdfrepo/sourcedata-transform:1.0
+docker login repo.xxx.com/scdfrepo
+docker push repo.xxx.com/scdfrepo/sourcedata-transform:1.0
 #Connect to shell and create stream
 java -jar spring-cloud-dataflow-shell-2.6.2.jar --spring.shell.commandFile=\
 register-app.txt,\
@@ -12,7 +12,7 @@ deploy-stream.txt
 
 #command dataflow config server http://10.216.52.96/
 #Register Image App
-#command app register --name mysource --type source --uri docker://harbor-repo.vmware.com/scdfrepo/sourcedata-transform:1.0
+#command app register --name mysource --type source --uri docker://repo.xxx.com/scdfrepo/sourcedata-transform:1.0
 #Register needed apps
 #app register --name jdbc --type source --uri docker:springcloudstream/jdbc-source-kafka:2.1.6.RELEASE
 #app register --name http --type source --uri docker:springcloudstream/http-source-kafka:2.1.4.RELEASE
